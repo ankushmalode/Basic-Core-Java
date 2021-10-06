dd3ea040e24f438274b7ef554e6a0b1729985dce
ankushmalode <you@example.com>
2021-10-06 09:42:52 +0530

    making indentation

--- src/main/java/com/Bridgelabz/FlipCoin.java
+++ src/main/java/com/Bridgelabz/FlipCoin.java
@@ -1,5 +1,5 @@
 /*
-  @author Ankush
+  @author 'Ankush'
  * @version 16.0
  * since 2021-09-28
  */
@@ -8,11 +8,14 @@ package com.Bridgelabz;
 import java.util.Scanner;
 
 public class FlipCoin {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         int Tail = 0, Head = 0;
         Scanner input = new Scanner(System.in);
         System.out.println("How many times do you want to flip the coin- ");
         int count = input.nextInt();
+        /*
+         * main logic implemented in for loop
+         */
         for (int i = 0; i < count; i++) {
             if (Math.random() < 0.5)
                 Tail++;
